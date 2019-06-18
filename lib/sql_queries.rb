3cc83c4de38ebccfdea0b68cc67d0829762efc97
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-18 15:48:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   # "SELECT COUNT(color) FROM bears WHERE MAX(color);"
-  "SELECT color FROM bears ORDER BY MAX(color);"
+  "SELECT color FROM bears ORDER BY COUNT(color);"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
