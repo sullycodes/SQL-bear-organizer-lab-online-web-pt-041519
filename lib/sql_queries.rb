e810db550177d4ec6555ba8881c245a9a5cc39c1
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-18 16:22:06 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,8 +19,8 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY color;"
-  # SELECT * FROM cats ORDER BY age DESC LIMIT 2;
+  # "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY color;"
+  "SELECT color, count(*) FROM bears GROUP BY color;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
